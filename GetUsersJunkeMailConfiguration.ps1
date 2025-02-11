0f96feed614463a884e437396fea57757be51555
ERTO <eros.toscano+github@gmail.com>
2025-02-11 23:17:57 +0100

    Get Users JunkMailConfiguration

--- GetUsersJunkeMailConfiguration.ps1
+++ GetUsersJunkeMailConfiguration.ps1
@@ -1,3 +1,8 @@
 Import-Module ExchangeOnlineManagement
 Get-MailboxJunkEmailConfiguration -Identity usersemailaddress@domain.com
 Get-MailboxJunkEmailConfiguration -Identity usersemailaddress@domain.com -BlockedSendersAndDomains @{remove="usersemailaddress@domain.com"}
+
+
+Disconnect-ExchangeOnline
+
+#viel Spass
